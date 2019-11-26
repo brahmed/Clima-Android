2f5a1d0352903de43ca332d63436081b48da9aed
brahmed <medamine.brah@gmail.com>
2019-11-26 11:23:25 +0100

    Complete App

---
+++ app/src/main/java/com/londonappbrewery/climapm/ChangeCityController.java
@@ -0,0 +1,42 @@
+package com.londonappbrewery.climapm;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class ChangeCityController extends AppCompatActivity {
+
+    EditText editTextFiel;
+    ImageButton backButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.change_city_layout);
+
+        editTextFiel = findViewById(R.id.queryET);
+        backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        editTextFiel.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                String newCity = editTextFiel.getText().toString();
+                Intent newCityIntent = new Intent(ChangeCityController.this, WeatherController.class);
+                newCityIntent.putExtra("City", newCity);
+                startActivity(newCityIntent);
+                return false;
+            }
+        });
+    }
+}
